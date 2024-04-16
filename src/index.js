b20e4f6b6aef5e723df704e6a18549d99fca4e66
unknown <Minskoleopc>
2024-04-16 19:44:09 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// npx create-react-app my-app
+// cd my-app
+// npm start
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Menu/>
+            <Footer />
+        </div>
+    );
+}
+
+// Part 3
+
+function Menu() {
+    return (
+        <div>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+        </div>
+    )
+}
+
+
+// part 1  
+// Defining a component 
+// Data (states and props) Logic Appearance 
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/funghi.jpg" alt="funghi-pizza" />
+            <h2>Pizza funghi</h2>
+            <p>Tomato Spinach and cheese</p>
+        </div>
+    );
+}
+
+// part 2
+function Footer() {
+    // how it works behind the scence - 'JSX'
+    // return React.createElement("footer",null,"burger bar 2019-2020")
+    // how to include javascript in jsx
+    return <footer>{new Date().toLocaleString()}BurgerBbar 2019-2020</footer>
+}
+
+
+function Header() {
+    return <h1>Welcome to the pizza club</h1>
+}
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />);
