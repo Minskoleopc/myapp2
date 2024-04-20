65a5f3d2395336a518f3221c576f6eb1662397fb
unknown <Minskoleopc>
2024-04-20 19:47:11 +0530

    added

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function App() {
     return (
         <div className="container">
             <Header />
-            <Menu/>
+            <Menu />
             <Footer />
         </div>
     );
@@ -49,7 +49,44 @@ function Footer() {
     // how it works behind the scence - 'JSX'
     // return React.createElement("footer",null,"burger bar 2019-2020")
     // how to include javascript in jsx
-    return <footer>{new Date().toLocaleString()}BurgerBbar 2019-2020</footer>
+    const hour = new Date().getHours()+5
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.log(isOpen)
+
+
+    function Order({closeHour,openHour}){
+        return (
+            <div className="order">
+                <p>
+                    We are open from {openHour}:00 to {closeHour}:00 Come visit us or order online.
+                </p>
+            </div>
+        )
+
+    }
+
+
+
+    return (
+        <footer className="footer">
+            {
+                isOpen ? (
+                    <Order closeHour={closeHour} openHour={openHour} />
+                ) : (
+                    <p>We are happy to welcome you between our open hr{openHour}:00 and {closeHour}:00</p>
+                )
+            }
+        </footer>
+    )
+
+
+
+
+
+
+
 }
 
 
