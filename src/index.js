3782be94167c646de6b7aac69a6495e81ace65d0
unknown <Minskoleopc>
2024-04-23 19:31:44 +0530

    addded

--- src/index.js
+++ src/index.js
@@ -2,11 +2,51 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-// npx create-react-app my-app
-// cd my-app
-// npm start
 
-console.log('day2')
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
 
 function App() {
     return (
@@ -21,53 +61,80 @@ function App() {
 // Part 3
 
 function Menu() {
+
+    let pizzas = pizzaData
+
+
     return (
-        <div>
-            <Pizza />
-            <Pizza />
-            <Pizza />
-        </div>
+
+        <main className="menu">
+            <h2>Our menu</h2>
+            <>
+                <p>
+                    Authentic Italian cuisine. 6 creative dishes to choose from. All
+                    from our stone oven, all organic, all delicious.
+                </p>
+                <ul className="pizzas">
+                    {
+                        pizzas.map((pizza) => (
+                            <Pizza PizzaObj={pizza} key={pizza.name} />
+                        ))
+                    }
+                </ul>
+            </>
+        </main>
+
     )
 }
 
 
-// part 1  
-// Defining a component 
-// Data (states and props) Logic Appearance 
-function Pizza() {
+
+function Pizza({ PizzaObj }) {
     return (
-        <div>
-            <img src="pizzas/funghi.jpg" alt="funghi-pizza" />
-            <h2>Pizza funghi</h2>
-            <p>Tomato Spinach and cheese</p>
-        </div>
+        <li className={`pizza ${PizzaObj.soldOut}?"sold-out":" "`}>
+            <img src={PizzaObj.photoName} alt={PizzaObj.name} />
+            <div>
+                <h3>{PizzaObj.name}</h3>
+                <p>{PizzaObj.ingredients}</p>
+                <span>{PizzaObj.soldOut ? "SOLD OUT" : PizzaObj.price}</span>
+            </div>
+        </li>
     );
 }
 
+
+
+// ORDER component
+function Order({ closeHour, openHour }) {
+    return (
+        <div className="order">
+            <p>
+                We are open from {openHour}:00 to {closeHour}:00 Come visit us or order online.
+            </p>
+        </div>
+    )
+
+}
+
+function Header() {
+    //const style = {color:"red"}
+    let style = {};
+    return (
+        <header className="header">
+            <h1 style={style}>Fast React Pizza Co.</h1>
+        </header>
+    )
+}
+
+
+
 // part 2
 function Footer() {
-    // how it works behind the scence - 'JSX'
-    // return React.createElement("footer",null,"burger bar 2019-2020")
-    // how to include javascript in jsx
-    const hour = new Date().getHours()+5
+
+    const hour = new Date().getHours()
     const openHour = 12;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
-    console.log(isOpen)
-
-
-    function Order({closeHour,openHour}){
-        return (
-            <div className="order">
-                <p>
-                    We are open from {openHour}:00 to {closeHour}:00 Come visit us or order online.
-                </p>
-            </div>
-        )
-
-    }
-
-
 
     return (
         <footer className="footer">
@@ -75,30 +142,18 @@ function Footer() {
                 isOpen ? (
                     <Order closeHour={closeHour} openHour={openHour} />
                 ) : (
-                    <p>We are happy to welcome you between our open hr{openHour}:00 and {closeHour}:00</p>
+                    <p>We are happy to welcome you between our open hr {openHour}:00 and {closeHour}:00</p>
                 )
             }
         </footer>
     )
 
 
-
-
-
-
-
 }
 
 
-function Header() {
-    //const style = {color:"red"}
-    let style = {}
-    return (
-        <header className="header">
-            <h1 style={style}>Fast React Pizza Co</h1>
-        </header>
-    )
-}
+
+
 
 
 
